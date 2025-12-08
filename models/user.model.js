86e61ceb7075a3038d4f3de919d10ef7d26c88a0
LudivineLeLan <lelanludivine@gmail.com>
2025-12-08 10:37:30 +0100

    user model & schema

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "./sequelize.client.js";
+
+export class User extends Model { }
+
+
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "user",
+  }
+);
