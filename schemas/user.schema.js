4733024690ca80c788c8cb1734e2658bd1891db4
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 11:57:38 +0100

    updated Joi Schema for password Regex

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,5 +3,5 @@ import Joi from "joi";
 export const UserSchema = Joi.object({
   name: Joi.string().trim().min(2).required(),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9!@#$%^&*]{3,30}$")).required()
 });
