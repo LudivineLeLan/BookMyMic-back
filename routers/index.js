8c1a2c381c03cae13ecae799a13980cc2803425f
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 10:44:24 +0100

    fixed index Router

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { bookingRouter } from "./booking.router.js";
 import { slotRouter } from "./slot.router.js";
+import { userRouter } from "./user.router.js";
+import { useActionState } from "react";
 
 export const apiRouter = Router();
 
 apiRouter.use("/bookings", bookingRouter);
 apiRouter.use("/slots", slotRouter);
+apiRouter.use("user", userRouter);
