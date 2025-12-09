7e8f9e2b40f04e14f06a1053d76a0433481ffb62
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 11:16:19 +0100

    fixed url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: ["https://bookmymic.onrender.com",
-    "http://localhost:3000"],
+    "http://localhost:3001"],
   methods: ["GET", "POST"],
 }));
 
