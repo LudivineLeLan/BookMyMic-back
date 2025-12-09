d220a7a5bf820c6ed16499bd76dd743b122c5f89
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 10:54:37 +0100

    deleted & fixed auto-insertion

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { bookingRouter } from "./booking.router.js";
 import { slotRouter } from "./slot.router.js";
 import { userRouter } from "./user.router.js";
-import { useActionState } from "react";
 
 export const apiRouter = Router();
 
