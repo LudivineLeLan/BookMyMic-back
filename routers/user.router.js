7bdd7ebd27bcafa02ee176a62bd5d4caa7cb30fe
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 11:45:59 +0100

    fixed routes user

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,6 +3,6 @@ import { userController } from "../controllers/user.controller.js";
 
 export const userRouter = Router();
 
-userRouter.post('/user/register', userController.createUser);
-userRouter.post('/user/login', userController.loginUser);
-userRouter.get('/user/:id/bookings', userController.getUserBookings)
+userRouter.post('/register', userController.createUser);
+userRouter.post('/login', userController.loginUser);
+userRouter.get('/:id/bookings', userController.getUserBookings)
