a3266e126a09ab31ce424fdc8f25fb73df2428ea
LudivineLeLan <lelanludivine@gmail.com>
2025-12-09 12:04:37 +0100

    updated Joi schema with user name

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const UserSchema = Joi.object({
-  name: Joi.string().trim().min(2).required(),
+  name: Joi.string().trim().min(2).pattern(/^[a-zA-ZÀ-ÿ\s'-]+$/).required(),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9!@#$%^&*]{3,30}$")).required()
 });
