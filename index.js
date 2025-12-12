6970e47d58b9ae422b99e6cf4175caf3b7eddb40
LudivineLeLan <lelanludivine@gmail.com>
2025-12-12 17:55:27 +0100

    fixes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/uploads', express.static('uploads'));
 
 app.use(apiRouter);
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve('path/to/build/index.html'));
 });
 
